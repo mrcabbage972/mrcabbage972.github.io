694c763a57a8dfee81bf58c784a68b87f0eb3a4d
mrcabbage972 <mayvic@gmail.com>
2025-07-18 12:09:42 +0000

    Initialize Hugo Academic site

---
+++ a.sh
@@ -0,0 +1,6 @@
+rm -rf .git
+git init
+git remote add origin https://github.com/mrcabbage972/mrcabbage972.github.io
+git add .
+git commit -m "Initialize Hugo Academic site"
+git push -u origin master  # or main
